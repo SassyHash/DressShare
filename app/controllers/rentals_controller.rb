87c46a0378a2ea6d03afb35d823871fbd6bdfd0c
@appacademy.io <@appacademy.io>
2013-02-26 18:06:25 -0800

    rentals form done but not saving, partials refactored well

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,8 +1,18 @@
 class RentalsController < ApplicationController
   def new
+    @dress = Dress.find(params[:dress_id])
+    @rental = Rental.new
+    @rental.dress_id = @dress.id
+    @rental.user_id = current_user.id
   end
 
   def create
+    @rental = Rental.new(:params[:rental])
+    if @rental.save
+      flash[:notices] = [] unless flash[:notices]
+      flash[:notices] << "You have requested to rent this dress."
+      redirect_to rental_path(@rental)
+    end
   end
 
   def edit
@@ -14,5 +24,10 @@ class RentalsController < ApplicationController
   def destroy
   end
 
+  def show
+  end
+
+  def index
+  end
 
 end
