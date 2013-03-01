3025142bc7483ec15411707210158abb99559b30
Brittany Yoon <binna.yoon@gmail.com>
2013-02-28 17:12:33 -0800

    Catching up on all the deletes

--- app/models/cart_dress.rb
+++
@@ -1,6 +0,0 @@
-class CartDress < ActiveRecord::Base
-  attr_accessible :cart_id, :dress_id
-
-  belongs_to :cart
-  belongs_to :dress
-end
--- test/unit/cart_dress_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CartDressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
