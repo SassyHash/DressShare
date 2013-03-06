1792c8c4bb415c4c397b39d8490e9d298f134357
Brittany Yoon <binna.yoon@gmail.com>
2013-03-05 23:20:01 -0800

    mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,19 @@ DressShare::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost' }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => "binna.yoon@gmail.com",
+    :password => "--------",
+    :domain => "sassyha.sh",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
