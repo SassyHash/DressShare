b680ebb0e58a74a5ec15613c44086090192899a1
Brittany Yoon <SassyHash@Brittanys-MacBook-Pro.local>
2013-04-28 20:05:53 -0400

    deleted that awful migration

--- db/migrate/20130417013521_change.rb
+++
@@ -1,6 +0,0 @@
-class ChangeSchoolName < ActiveRecord::Migration
-  def change
-    drop_column :schools, :school_name
-    add_column :schools, :name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -106,10 +106,10 @@ ActiveRecord::Schema.define(:version => 20130417013718) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "class_year"
     t.integer  "failed_attempts",        :default => 0
     t.string   "unlock_token"
-    t.time     "locked_at"
+    t.datetime "locked_at"
+    t.integer  "class_year"
     t.integer  "school_id"
   end
 
