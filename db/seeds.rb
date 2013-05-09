96054de2fbecf6437b61ac152838e85e30926758
Sassy <binna.yoon@gmail.com>
2013-05-09 18:39:10 -0400

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 LABELS = ["Petite", "Athletic", "Lean", "Busty", "Pear", "Tall", "Hourglass", "Tummy-Shaping", "Curvy", "Conservative"]
   LABELS.each { |label| BodyType.create(label: label) }
 
+School.create(:town => "Hanover", :state => "NH", :domain =>"dartmouth.edu", :name => "Dartmouth College");
+	School.create(:town => "Lakeville", :state => "CT", :domain =>"hotchkiss.org", :name => "Hotchkiss School");
+School.create(:town => "Middlebury", :state => "VT", :domain =>"middlebury.edu", :name => "Middlebury College");
+School.create(:town => "Berkeley", :state => "CA", :domain =>"berkeley.edu", :name => "UC Berkeley");
