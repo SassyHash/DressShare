6105e6d0a08d6a44ea8a0b17b74f936181860172
Sassy <binna.yoon@gmail.com>
2013-05-09 11:27:20 -0400

    first facebook gate auth working

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '583452738346072', 'c51f1dd5164389f8223b06558bf5cfe9', 
-  :scope => "email, user_birthday, first_name, last_name", :display => 'popup'
+  provider :facebook, '583452738346072', 'c51f1dd5164389f8223b06558bf5cfe9',
+  :scope => 'email, user_birthday, user_education_history'
 end
