717cd42551c59669107478a8c114b969f7efd1cb
Sassy <binna.yoon@gmail.com>
2013-05-19 22:35:14 -0400

    my milkshake brings all the boys to the yard

--- app/controllers/pool_controller.rb
+++ app/controllers/pool_controller.rb
@@ -1,9 +1,15 @@
 class PoolController < ApplicationController
 
 	def new
+		@pool = Pool.new()
 	end
 
 	def create
+		@pool = current_user.pools.build(params[:pool])
+		if @pool.save
+			flash[:notices] = "You have made a new dress pool!"
+			redirect_to pool_path(@pool)
+		end
 	end
 
 	def edit
